a8988c90968bda51c61ca5fadfadbe4f0a759804
SUMIT <57702543+SumitSMARTER@users.noreply.github.com>
2020-07-15 07:51:16 +0530

    Done

--- Trivia App/Controllers/HistoryVC.swift
+++ Trivia App/Controllers/HistoryVC.swift
@@ -15,8 +15,8 @@ class HistoryVC: UIViewController {
     
     var histData = HistoryModel.historyDict
     
-    var arrayOfNames = [String]()
-    var arrayOfDates = [String]()
+    var arrayOfQuess = [String]()
+    var arrayOfAns = [String]()
     var gameCount = 0
     
     override func viewDidLoad() {
@@ -25,16 +25,19 @@ class HistoryVC: UIViewController {
         Donebtn.layer.cornerRadius = 15
         
         
-        for i in 0..<histData.count {
-             let name = histData[i]["Q&A"]
-            print("hisNAME",name)
+        if let fetchD = histData[0] as? [String: Any] {
             
-//            for i in 0..<name.count {
-//                
-//            }
-       
+            let QU = fetchD["Q&A"] as! Array<Any>
+                 
+                 for i in 0..<QU.count {
+                     let ques = (QU[i] as AnyObject)["Question"] as! String
+                     let ans = (QU[i] as AnyObject)["Answer"] as! String
+                     
+                    print("ques", ques , ans)
+                     arrayOfQuess.append(ques)
+                     arrayOfAns.append(ans)
+                 }
         }
-        
         
         
     }
@@ -49,22 +52,21 @@ class HistoryVC: UIViewController {
 
 
 extension HistoryVC: UITableViewDelegate, UITableViewDataSource {
-    
+
     func numberOfSections(in tableView: UITableView) -> Int {
         return histData.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return histData.count
+        return arrayOfQuess.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Q_A_TableViewCell") as! Q_A_TableViewCell
         
-//        cell.question.text  = histData[indexPath.item]["Question"]![indexPath.row] as! String
-        //        cell.answer.text    = histData[indexPath.row]["Answer"] as! String
+        cell.question.text  =  arrayOfQuess[indexPath.row]
+        cell.answer.text    = "Answer: " + arrayOfAns[indexPath.row]
         
         return cell
         
@@ -74,11 +76,10 @@ extension HistoryVC: UITableViewDelegate, UITableViewDataSource {
         
         gameCount += 1
         
-        print("INDSIDE", arrayOfDates, arrayOfNames)
         let cell = tableView.dequeueReusableCell(withIdentifier: "historyTableViewCell") as! historyTableViewCell
-        cell.name.text = histData[section]["name"] as! String
+        cell.name.text = histData[section]["name"] as? String
         cell.gameNo.text = "GAME " + "\(gameCount)"
-        cell.dateTime.text = histData[section]["date"] as! String
+        cell.dateTime.text = histData[section]["date"] as? String
         return cell
         
     }
